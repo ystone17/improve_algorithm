5cd75abbd033f17c86462487ca8691b2fcaef318
sjhan2781 <sjhan2781@gmail.com>
2021-03-29 21:53:48 +0900

    sangjin: 47번 코드 수정

--- sangjin/level1/47_행렬의_덧셈.cpp
+++ sangjin/level1/47_행렬의_덧셈.cpp
@@ -8,7 +8,7 @@ vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2)
 
     for(int i = 0; i < arr1.size(); i++){
         vector<int> tmp;
-        for(int j = 0; j < arr1.size(); j++){
+        for(int j = 0; j < arr1[0].size(); j++){
             tmp.push_back(arr1[i][j] + arr2[i][j]);
         }
         answer.push_back(tmp);
